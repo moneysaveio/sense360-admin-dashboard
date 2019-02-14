ee730afdfe7ba3984652934b2df5769b118f2cf0
Kaushik Varanasi <kaushik.varanasi1@gmail.com>
2019-02-15 05:28:16 +0530

    [ADDED] dummy data - added dummy data file

---
+++ src/app/routes/sensorEvents/data.js
@@ -0,0 +1,85 @@
+import React from 'react';
+
+// Sensor Events data
+export const sensorEventData = [
+    {
+        id: 5,
+        type: "AirConditioner",
+        sensorId: "AC-01-1.5T",
+        dataPacket: {
+            timestamp: "2018.12.30 AD at 17:07:38 IST",
+            actualCurrent: "0",
+            temprature: "23",
+            humidity: "46",
+            compressorStatus: "ON",
+            overloadCurrent: "0",
+        },
+        thresholds: {
+            actualCurrent: "none",
+            temprature: "higher",
+            humidity: "lower",
+            compressorStatus: "ON",
+            overloadCurrent: "none",
+        }
+    },
+    {
+        id: 6,
+        type: "AirConditioner",
+        sensorId: "AC-01-1.5T",
+        dataPacket: {
+            timestamp: "2018.12.30 AD at 17:07:38 IST",
+            actualCurrent: "0",
+            temprature: "23",
+            humidity: "46",
+            compressorStatus: "ON",
+            overloadCurrent: "0",
+        },
+        thresholds: {
+            actualCurrent: "none",
+            temprature: "higher",
+            humidity: "lower",
+            compressorStatus: "ON",
+            overloadCurrent: "none",
+        }
+    },
+    {
+        id: 7,
+        type: "AirConditioner",
+        sensorId: "AC-01-1.5T",
+        dataPacket: {
+            timestamp: "2018.12.30 AD at 17:07:38 IST",
+            actualCurrent: "0",
+            temprature: "23",
+            humidity: "46",
+            compressorStatus: "ON",
+            overloadCurrent: "0",
+        },
+        thresholds: {
+            actualCurrent: "none",
+            temprature: "higher",
+            humidity: "lower",
+            compressorStatus: "ON",
+            overloadCurrent: "none",
+        }
+    },
+    {
+        id: 8,
+        type: "AirConditioner",
+        sensorId: "AC-01-1.5T",
+        dataPacket: {
+            timestamp: "2018.12.30 AD at 17:07:38 IST",
+            actualCurrent: "0",
+            temprature: "23",
+            humidity: "46",
+            compressorStatus: "ON",
+            overloadCurrent: "0",
+        },
+        thresholds: {
+            actualCurrent: "none",
+            temprature: "higher",
+            humidity: "lower",
+            compressorStatus: "ON",
+            overloadCurrent: "none",
+        }
+    },
+];
